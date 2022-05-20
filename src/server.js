ee5b99d188149f4ddc7757d124448913e650b763
BrunoUemura <bruno.h.uemura@outlook.com>
2022-05-20 01:27:34 -0300

    feat: Implementing middlewares

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
-const { app } = require('./');
+const { app } = require("./");
 
-app.listen(3333);
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
